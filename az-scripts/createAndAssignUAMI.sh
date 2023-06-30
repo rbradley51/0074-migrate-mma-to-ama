86c02b3a5bc1bb54e5ca6abed8615e0048f12391
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-06-29 22:48:19 -0400

    user-assigned(managed-identity): create, add roles

---
+++ az-scripts/createAndAssignUAMI.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script creates a new user-assigned managed identity and assignes it to a set of roles on a given scope.
+roles=("Virtual Machine Contributor" "Log Analytics Contributor" "Azure Arc Connected Resource Administrator")
+az group create -n $uamiResourceGroup -l $uamiLocation;
+rgId=$(az group show --name $uamiResourceGroiup --query "id" -o tsv)
+az identity create -g $uamiResourceGroup -n $uamiName --query id -o tsv;
+for role in "${roles[@]}";
+   do echo $role;
+   az role assignment create --role "$role" --assignee $uamiName --scope $rgId;
+done
