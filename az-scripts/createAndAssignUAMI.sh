8b2bc8ff5d14b1d214214883e9f33cb6953439a5
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-06-29 22:52:45 -0400

    resource(group): mispelled

--- az-scripts/createAndAssignUAMI.sh
+++ az-scripts/createAndAssignUAMI.sh
@@ -2,7 +2,7 @@
 # This script creates a new user-assigned managed identity and assignes it to a set of roles on a given scope.
 roles=("Virtual Machine Contributor" "Log Analytics Contributor" "Azure Arc Connected Resource Administrator")
 az group create -n $uamiResourceGroup -l $uamiLocation;
-rgId=$(az group show --name $uamiResourceGroiup --query "id" -o tsv)
+rgId=$(az group show --name $uamiResourceGroup --query "id" -o tsv)
 az identity create -g $uamiResourceGroup -n $uamiName --query id -o tsv;
 for role in "${roles[@]}";
    do echo $role;
