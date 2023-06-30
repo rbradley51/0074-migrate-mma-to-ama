59ac5e9398f9e8040477dc88c6e13ae44ec36bce
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-06-29 23:38:20 -0400

    subscription(id): set to sub01

--- az-scripts/createAndAssignUAMI.sh
+++ az-scripts/createAndAssignUAMI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script creates a new user-assigned managed identity and assignes it to a set of roles on a given scope.
 roles=("Virtual Machine Contributor" "Log Analytics Contributor" "Azure Connected Machine Resource Administrator")
-# az account set -s $AZURE_SUBSCRIPTION_ID
+az account set -s $AZURE_SUBSCRIPTION_ID
 az group create -n $uamiResourceGroup -l $uamiLocation;
 rgId=$(az group show --name $uamiResourceGroup --query "id" -o tsv)
 az identity create -g $uamiResourceGroup -n $uamiName --query id -o tsv;
