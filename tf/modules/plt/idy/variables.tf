7ee18a71c444f980cc58a2d283a00d5af268e503
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-07-03 23:28:16 -0400

    idy(var): use for settings

--- tf/modules/plt/idy/variables.tf
+++ tf/modules/plt/idy/variables.tf
@@ -7,7 +7,7 @@
 #   type = string
 #   default = "idy-rgp"
 # }
-  
+
 # variable "vntName" {
 #   type = string
 #   default = "ads-vnt"
@@ -42,7 +42,7 @@
 #   type = string
 #   default = "ads01-nic-config"
 # }
-  
+
 # variable "prvIpAlloc" {
 #   type = string
 #   default = "Dynamic"
@@ -77,7 +77,7 @@
 #     type = string
 #     default = "latest"
 # }
-  
+
 # variable "osDiskName" {
 #     type = string
 #     default = "syst"
@@ -87,7 +87,7 @@
 #     type = string
 #     default = "ReadWrite"
 # }
-  
+
 # variable "createOption" {
 #     type = string
 #     default = "FromImage"
@@ -108,10 +108,69 @@
 #     default = true
 # }
 
-variable "configure_identity_resources" {
+variable "idy" {
   type = object({})
+  default = {
+    settings = {
+      identity = {
+        config = {
+          primary_location = "centralus"
+          rgpName          = "idy-rgp"
+          sta = {
+            kind             = "StorageV2"
+            tier             = "Standard"
+            replication_type = "LRS"
+          }
+          kvt = {
+            sku = "standard"
+          }
+          tags = {
+            key   = "environment"
+            value = "Production"
+          }
+          vnet = {
+            vntName       = "idy-vnt"
+            vntAddrSpaces = ["10.0.0.0/28"]
+            subnet = {
+              nsgName       = "ads-nsg"
+              sntName       = "ads-snt"
+              subAddrPrefix = "10.0.0.0/29"
+              nsgName       = "ads-nsg"
+              nic = {
+                ads01NicName       = "ads01-nic"
+                ads01NicConfigName = "ads01-nic-config"
+                prvIpAlloc         = "Dynamic"
+              }
+            }
+          }
+          image = {
+            publisher = "MicrosoftWindowsServer"
+            offer     = "WindowsServer"
+            sku       = "2022-Datacenter"
+            version   = "latest"
+          }
+          disk = {
+            osDiskName   = "syst"
+            caching      = "ReadWrite"
+            createOption = "FromImage"
+            diskType     = "Standard_LRS"
+          }
+          avset = {
+            avsetName = "AvSetADS"
+          }
+          vm = {
+            userName         = "adsadmin"
+            pw               = ""
+            provisionVmAgent = true
+            vmName           = "azrads01"
+            vmSize           = "Standard_DS2_v2"
+          }
+        }
+      }
+    }
+  }
 }
 variable "pw" {
-    type = string
-    default = ""
+  type    = string
+  default = ""
 }
