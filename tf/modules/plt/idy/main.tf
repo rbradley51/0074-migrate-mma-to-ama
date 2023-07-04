76b6ad484a69b0fa86951d578144692495c0eeb5
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-07-03 23:12:49 -0400

    identity(resources): add ref to main

--- tf/modules/plt/idy/main.tf
+++ tf/modules/plt/idy/main.tf
@@ -6,19 +6,20 @@ provider "azurerm" {
 resource "random_uuid" "rnd" {
 }
 
-idy = local.configure_identity_resources
+configure_identity_resources = local.configure_identity_resources
+
 resource "azurerm_recovery_services_vault" "rsv" {
   name                = "rsv-${random_uuid.rnd.result}"
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
 }
 
 resource "azurerm_key_vault" "kvt" {
   name = "kvt-${random_uuid.rnd.result}"
-  location = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
-  sku_name = idy.settings.identity.config.kvt.sku
-  tenant_id = idy.settings.identity.config.tenant_id
+  location = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
+  sku_name = configure_identity_resources.settings.identity.config.kvt.sku
+  tenant_id = configure_identity_resources.settings.identity.config.tenant_id
   soft_delete_enabled = true
   purge_protection_enabled = false
   enabled_for_disk_encryption = false
@@ -28,88 +29,88 @@ resource "azurerm_key_vault" "kvt" {
 }
 
 resource "azurerm_resource_group" "idy" {
-  name     = idy.settings.identity.config.rgpName 
-  location = idy.settings.identity.config.primary_location
+  name     = configure_identity_resources.settings.identity.config.rgpName 
+  location = configure_identity_resources.settings.identity.config.primary_location
 }
 
 resource "azurerm_storage_account" "idy" {
   account_kind = "StorageV2"
   account_tier = "Standard"
   account_replication_type = "LRS"
-  resource_group_name = idy.settings.identity.config.rgpName
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
 
 }
 
 resource "azurerm_network_security_group" "idy" {
-  name                = idy.settings.identity.config.vnet.subnet.nsgName
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
+  name                = configure_identity_resources.settings.identity.config.vnet.subnet.nsgName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
 }
 resource "azurerm_virtual_network" "idy" {
-  name                = idy.settings.identity.config.vnet.vntName
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
-  address_space       = idy.settings.identity.config.vnet.vntAddrSpaces # ["10.0.0.0/28"]
+  name                = configure_identity_resources.settings.identity.config.vnet.vntName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
+  address_space       = configure_identity_resources.settings.identity.config.vnet.vntAddrSpaces # ["10.0.0.0/28"]
   # dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   subnet {
-    name           = idy.settings.identity.config.vnet.subnet.sntName
-    address_prefix = idy.settings.identity.config.vnet.subnet.subAddrPrefix # ["10.0.0.0/29"]
+    name           = configure_identity_resources.settings.identity.config.vnet.subnet.sntName
+    address_prefix = configure_identity_resources.settings.identity.config.vnet.subnet.subAddrPrefix # ["10.0.0.0/29"]
     security_group = azurerm_network_security_group.idy.id  
 }
 
   tags = {
-    idy.settings.identity.config.tags.environment.key = idy.settings.identity.config.tags.environment.value
+    configure_identity_resources.settings.identity.config.tags.environment.key = configure_identity_resources.settings.identity.config.tags.environment.value
   }
 }
 
 resource "azurerm_network_interface" "ads01" {
-  name                = idy.settings.identity.config.vnet.subnet.nic.ads01NicName
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
+  name                = configure_identity_resources.settings.identity.config.vnet.subnet.nic.ads01NicName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
 
   ip_configuration {
-    name                          = idy.settings.identity.config.vnet.subnet.nic.ads01NicConfigName
+    name                          = configure_identity_resources.settings.identity.config.vnet.subnet.nic.ads01NicConfigName
     # https://stackoverflow.com/questions/56861532/how-to-reference-objects-in-terraform
     subnet_id                     = azurerm_virtual_network.idy.subnet.*.id[0]
-    private_ip_address_allocation = idy.settings.identity.config.vnet.subnet.nic.prvIpAlloc
+    private_ip_address_allocation = configure_identity_resources.settings.identity.config.vnet.subnet.nic.prvIpAlloc
   }
 }
 
 resource "azurerm_availability_set" "avs01" {
-  name                = idy.settings.identity.config.avset.avsetName
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
+  name                = configure_identity_resources.settings.identity.config.avset.avsetName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
 }
 resource "azurerm_virtual_machine" "ads01" {
-  name                  = idy.settings.identity.config.vm.vmName
-  location            = idy.settings.identity.config.primary_location
-  resource_group_name = idy.settings.identity.config.rgpName
+  name                  = configure_identity_resources.settings.identity.config.vm.vmName
+  location            = configure_identity_resources.settings.identity.config.primary_location
+  resource_group_name = configure_identity_resources.settings.identity.config.rgpName
   network_interface_ids = [azurerm_network_interface.ads01.id]
-  vm_size               = idy.settings.identity.config.vm.vmSize
+  vm_size               = configure_identity_resources.settings.identity.config.vm.vmSize
   availability_set_id   = azurerm_availability_set.avs01.id
 
   storage_image_reference {
-    publisher = idy.settings.identity.config.vm.vmName 
-    offer     = idy.settings.identity.config.image.publisher 
-    sku       = idy.settings.identity.config.image.sku 
-    version   = idy.settings.identity.config.image.version
+    publisher = configure_identity_resources.settings.identity.config.vm.vmName 
+    offer     = configure_identity_resources.settings.identity.config.image.publisher 
+    sku       = configure_identity_resources.settings.identity.config.image.sku 
+    version   = configure_identity_resources.settings.identity.config.image.version
   }
 
   storage_os_disk {
-    name              = idy.settings.identity.config.disk.osDiskName
-    caching           = idy.settings.identity.config.disk.caching
-    create_option     = idy.settings.identity.config.disk.createOption
-    managed_disk_type = idy.settings.identity.config.disk.diskType
+    name              = configure_identity_resources.settings.identity.config.disk.osDiskName
+    caching           = configure_identity_resources.settings.identity.config.disk.caching
+    create_option     = configure_identity_resources.settings.identity.config.disk.createOption
+    managed_disk_type = configure_identity_resources.settings.identity.config.disk.diskType
   }
 
   os_profile {
-    computer_name  = idy.settings.identity.config.vm.vmName
-    admin_username = idy.settings.identity.config.vm.userName
+    computer_name  = configure_identity_resources.settings.identity.config.vm.vmName
+    admin_username = configure_identity_resources.settings.identity.config.vm.userName
     admin_password = var.pw
   }
 
   os_profile_windows_config {
-    provision_vm_agent = idy.settings.identity.config.vm.provisionVmAgent
+    provision_vm_agent = configure_identity_resources.settings.identity.config.vm.provisionVmAgent
   }
 }
