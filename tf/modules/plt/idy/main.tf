a0a0a6311df48775728d02cfc91c62b15ed9cf9a
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-07-20 12:05:06 -0400

    nic(indices): align w/subnet ids

--- tf/modules/plt/idy/main.tf
+++ tf/modules/plt/idy/main.tf
@@ -81,7 +81,7 @@ resource "azurerm_network_interface" "idy" {
   resource_group_name = azurerm_resource_group.idy.name
   ip_configuration {
     name = var.idy_nics[count.index].ipconfig
-    subnet_id = (var.idy_nics[count.index].name == "svr01-nic" ? azurerm_virtual_network.idy.subnet.*.id[1] : azurerm_virtual_network.idy.subnet.*.id[count.index])
+    subnet_id = ("${count.index}" == 2 ? azurerm_virtual_network.idy.subnet.*.id[1] : azurerm_virtual_network.idy.subnet.*.id[0])
     # https://stackoverflow.com/questions/56861532/how-to-reference-objects-in-terraform
     private_ip_address_allocation = var.idy_nics[count.index].prvIpAlloc
     private_ip_address            = var.idy_nics[count.index].prvIpAddr
@@ -121,7 +121,7 @@ resource "azurerm_virtual_machine" "vms" {
   name                  = var.vms[count.index].vmName
   location              = var.primary_location
   resource_group_name   = azurerm_resource_group.idy.name
-  network_interface_ids = (var.idy_nics[count.index].name == "svr01-nic" ? azurerm_network_interface.idy[2].*.id : azurerm_network_interface.idy[count.index].*.id)
+  network_interface_ids = ("${count.index}" == 2 ? azurerm_network_interface.idy[2].*.id : azurerm_network_interface.idy[count.index].*.id)
   vm_size               = var.vms[count.index].vmSize
   availability_set_id   = ("${count.index}" == 1 ? azurerm_availability_set.avs_idy[1].id : azurerm_availability_set.avs_idy[0].id)
 
