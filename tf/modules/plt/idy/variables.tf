2322630238eac0f6f63a80020c43d8f796ac7109
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-07-20 10:56:56 -0400

    example(resource): remove [skip ci]

--- tf/modules/plt/idy/variables.tf
+++ tf/modules/plt/idy/variables.tf
@@ -419,18 +419,3 @@ variable "bastion" {
     }
   }
 }
-
-resource "azurerm_bastion_host" "example" {
-  name                = "examplebastion"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.example.id
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
-}
-
-# AzureBastionSubnet
-# 10.0.0.64/26
