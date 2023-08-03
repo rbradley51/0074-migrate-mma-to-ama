82a2ae9296601935f2927cbbc8b721e969f91ed0
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-03 19:14:19 -0400

    endpoints(public): test

--- tf/modules/plt/idy/main.tf
+++ tf/modules/plt/idy/main.tf
@@ -441,17 +441,20 @@ resource "azurerm_network_connection_monitor" "idy" {
 
   endpoint {
     name    = "dest-global-handler"
-    address = "global.handler.control.monitor.azure.com"
+    # address = "global.handler.control.monitor.azure.com"
+    address = "microsoft.com"
   }
 
   endpoint {
     name    = "dest-regional-handler"
-    address = "${var.primary_location}.handler.control.monitor.azure.com"
+    # address = "${var.primary_location}.handler.control.monitor.azure.com"
+    address = "google.com"
   }
 
   endpoint {
     name    = "dest-law-endpoint"
-    address = "${azurerm_log_analytics_workspace.law[0].id}.ods.opinsights.azure.com"
+    # address = "${azurerm_log_analytics_workspace.law[0].id}.ods.opinsights.azure.com"
+    address = "amazon.com"
   }
 
   test_configuration {
