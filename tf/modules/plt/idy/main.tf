f44c3a73f103cf92d59890f3fddaeccb64820673
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-09 20:12:19 -0400

    type(handler): update version

--- tf/modules/plt/idy/main.tf
+++ tf/modules/plt/idy/main.tf
@@ -510,7 +510,7 @@ resource "azurerm_virtual_machine_extension" "mde_test_windows" {
   virtual_machine_id   = azurerm_virtual_machine.vms[2].id 
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
-  type_handler_version = "1.10.16"
+  type_handler_version = "1.10"
   auto_upgrade_minor_version = true
 
   settings = <<SETTINGS
