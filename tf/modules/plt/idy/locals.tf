df5bbb6e953abefe13c7030a7a888ec96684716c
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-14 19:15:18 -0400

    add(computer): use credentials parameter

--- tf/modules/plt/idy/locals.tf
+++ tf/modules/plt/idy/locals.tf
@@ -11,7 +11,7 @@ locals {
   mods_3 = "Import-Module -Name ADDSDeployment, DnsServer"
   forest_4 = "Install-ADDSForest -DomainName ${var.domain.fqdn} -DomainNetbiosName ${var.domain.netbios} -DomainMode ${var.domain.mode} -ForestMode ${var.domain.mode} -DatabasePath ${var.domain.database_path} -SysvolPath ${var.domain.sysvol_path} -LogPath ${var.domain.log_path} -NoRebootOnCompletion:$false -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString ${var.pw} -AsPlainText -Force)"
   powershell = "${local.adds_1}; ${local.dns_2}; ${local.mods_3}; ${local.forest_4}"
-  join = "Add-Computer -DomainName ${var.domain.fqdn} -Restart"
+  join = "Add-Computer -DomainName ${var.domain.fqdn} -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ${var.admin_username}@${var.domain.fqdn}, (ConvertTo-SecureString -String ${var.pw} -AsPlainText -Force)  -Restart"
   promote = "Install-ADDSDomainController -DomainName ${var.domain.fqdn} -SafeModeAdministratorPassword (ConvertTo-SecureString ${var.pw} -AsPlainText -Force) -NoGlobalCatalog:$false -InstallDns:$true -Force:$true -NoRebootOnCompletion:$false -Force:$true"
   addDC = "${local.join}; ${local.promote}"
 }
