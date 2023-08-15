da42c8e6c2250ea9081fa597baa06bd88df4f432
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-15 16:49:17 -0400

    register(dns-client): remove force parameter

--- tf/modules/plt/idy/locals.tf
+++ tf/modules/plt/idy/locals.tf
@@ -13,7 +13,7 @@ locals {
   powershell = "${local.adds_1}; ${local.dns_2}; ${local.mods_3}; ${local.forest_4}"
   updateDNS = "Set-DnsClientGlobalSetting -SuffixSearchList ${var.domain.fqdn} -PassThru -Confirm:$false -Verbose"
   clearDNS = "Clear-DnsClientCache -Confirm:$false"
-  registerDNS = "Register-DnsClient -Confirm:$false -Force:$true"
+  registerDNS = "Register-DnsClient -Confirm:$false"
   join = "Add-Computer -DomainName ${var.domain.fqdn} -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList adsadmin@${var.domain.fqdn}, (ConvertTo-SecureString -String ${var.pw} -AsPlainText -Force)) -Restart"
   promote = "Install-ADDSDomainController -DomainName ${var.domain.fqdn} -SafeModeAdministratorPassword (ConvertTo-SecureString ${var.pw} -AsPlainText -Force) -NoGlobalCatalog:$false -InstallDns:$true -Force:$true -NoRebootOnCompletion:$false -Force:$true"
   joinServer = "${local.updateDNS}; ${local.clearDNS}; ${local.registerDNS} ${local.join}"
