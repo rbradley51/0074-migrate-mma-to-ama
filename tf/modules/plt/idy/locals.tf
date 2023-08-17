816bb6097e248aa22f78cb7cc615de9ec2e7afce
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-17 15:57:49 -0400

    dc2(ps): adjust command syntax

--- tf/modules/plt/idy/locals.tf
+++ tf/modules/plt/idy/locals.tf
@@ -23,7 +23,7 @@ locals {
   dc2password_command     = "$password = ConvertTo-SecureString ${var.pw} -AsPlainText -Force"
   dc2creds_command        = "$mycreds = New-Object System.Management.Automation.PSCredential -ArgumentList $dc2user, $password"
   dc2install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
-  dc2configure_ad_command = "Install-ADDSDomainController -Credential $mycreds -CreateDnsDelegation:$false -DomainName ${var.domain.fqdn} -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  dc2configure_ad_command = "Install-ADDSDomainController -DomainName ${var.domain.fqdn} -SafeModeAdministratorPassword $password -Credential $mycreds -CreateDnsDelegation:$false -InstallDns:$true -Confirm:$false -Force:$true"
   dc2shutdown_command     = "shutdown -r -t 10"
   dc2exit_code_hack       = "exit 0"
   dc2powershell_command   = "${local.dc2import_command}; ${local.dc2user_command}; ${local.dc2password_command}; ${local.dc2creds_command}; ${local.dc2install_ad_command}; ${local.dc2configure_ad_command}; ${local.dc2shutdown_command}; ${local.dc2exit_code_hack}"
