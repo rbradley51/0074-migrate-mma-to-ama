e7a61a1eb144b262010af20b4f42e99a5d07b990
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-08-25 14:06:52 -0400

    delay(command): wait for 1 min [skip ci]

--- tf/modules/plt/idy/locals.tf
+++ tf/modules/plt/idy/locals.tf
@@ -8,9 +8,10 @@ locals {
   test_mde = false
   adds_1 = "Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools"
   dns_2 = "Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools"
+  dc1_waitOneMin = "Start-Sleep -s 60"
   mods_3 = "Import-Module -Name ADDSDeployment, DnsServer"
   forest_4 = "Install-ADDSForest -DomainName ${var.domain.fqdn} -DomainNetbiosName ${var.domain.netbios} -DomainMode ${var.domain.mode} -ForestMode ${var.domain.mode} -DatabasePath ${var.domain.database_path} -SysvolPath ${var.domain.sysvol_path} -LogPath ${var.domain.log_path} -NoRebootOnCompletion:$false -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString ${var.pw} -AsPlainText -Force)"
-  powershell = "${local.adds_1}; ${local.dns_2}; ${local.mods_3}; ${local.forest_4}"
+  powershell = "${local.adds_1}; ${local.dns_2}; ${local.dc1_waitOneMin}; ${local.mods_3}; ${local.forest_4}"
   updateDNS = "Set-DnsClientGlobalSetting -SuffixSearchList ${var.domain.fqdn} -PassThru -Confirm:$false -Verbose"
   clearDNS = "Clear-DnsClientCache -Confirm:$false"
   registerDNS = "Register-DnsClient -Confirm:$false"
