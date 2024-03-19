558339ca6b1fcf243fa3a73b3945c46d160b246d
autocloudarc <autocloudarc@outlook.com>
2024-03-19 08:56:20 -0400

    env(prefix): add to rgpname

--- az-scripts/Get-DriveSpace.ps1
+++ az-scripts/Get-DriveSpace.ps1
@@ -2,6 +2,6 @@ $vmList = (Get-AzVM -ResourceGroupName $env:rgpName).Name
 ForEach ($vmName in $vmList) 
 {
     $SystemFreeSpaceGt10GB = "((Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -match $env:SystemDrive[0] }).Free/1GB -gt 10)"
-    Invoke-AzVMRunCommand -ResourceGroupName $rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpaceGt10GB -Verbose
+    Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpaceGt10GB -Verbose
 }
 
