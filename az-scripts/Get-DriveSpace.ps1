99a7eb124f728c10863f52dafb459cd2d5860a07
autocloudarc <autocloudarc@outlook.com>
2024-03-19 09:48:05 -0400

    vm(os): select

--- az-scripts/Get-DriveSpace.ps1
+++ az-scripts/Get-DriveSpace.ps1
@@ -1,7 +1,15 @@
 $vmList = (Get-AzVM -ResourceGroupName $env:rgpName).Name
 ForEach ($vmName in $vmList) 
 {
-    $SystemFreeSpaceInGB = "((Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -match $env:SystemDrive[0] }).Free/1GB)"
+    $os = Get-AzVM -ResourceGroupName $env:rgpName -Name $vmName | Select-Object -ExpandProperty StorageProfile | Select-Object -ExpandProperty OsDisk | Select-Object -ExpandProperty OsType
+    if ($os -eq "Windows")
+    {
+        $SystemFreeSpaceInGB = "((Get-PSDrive -PSProvider FileSystem -Name 'C').Free/1GB)"
+    }
+    else
+    {
+        $SystemFreeSpaceInGB = "((Get-PSDrive -PSProvider FileSystem -Name '/').Free/1GB)"
+    }
     $output = Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpaceInGB
     $output.Value[0].Message
 }
