d4afd45724061d622d4db7fe046d4368436a713c
autocloudarc <autocloudarc@outlook.com>
2024-03-19 09:03:16 -0400

    system(freespace): show value for ps script

--- az-scripts/Get-DriveSpace.ps1
+++ az-scripts/Get-DriveSpace.ps1
@@ -1,7 +1,9 @@
 $vmList = (Get-AzVM -ResourceGroupName $env:rgpName).Name
 ForEach ($vmName in $vmList) 
 {
-    $SystemFreeSpaceGt10GB = "((Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -match $env:SystemDrive[0] }).Free/1GB -gt 10)"
-    Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpaceGt10GB -Verbose
+    # $SystemFreeSpaceGt10GB = "((Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -match $env:SystemDrive[0] }).Free/1GB -gt 10)"
+    $SystemFreeSpace = "((Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -match $env:SystemDrive[0] }).Free/1GB)"
+    Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpace -Verbose
+    # Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $SystemFreeSpaceGt10GB -Verbose
 }
 
