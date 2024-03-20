3da1c862b05a223edb51d7f33a29de018e93b087
autocloudarc <autocloudarc@outlook.com>
2024-03-19 21:57:48 -0400

    zero(index): remove from value

--- az-scripts/Get-EndpointConnectionResult.ps1
+++ az-scripts/Get-EndpointConnectionResult.ps1
@@ -8,13 +8,13 @@ $endpointList = @(
 $vmList = (Get-AzVM -ResourceGroupName $env:rgpName).Name
 ForEach ($vmName in $vmList) 
 {
-    Write-Host ("="*100)
+    Write-Host ("="*150)
     ForEach ($endpoint in $endpointList)
     {
-        Write-Host ("-"*100)
+        Write-Host ("-"*150)
         $ScriptString = "(Test-Connection -ComputerName $endpoint -Count 1).Status"
         $output = Invoke-AzVMRunCommand -ResourceGroupName $env:rgpName -Name $vmName -CommandId 'RunPowerShellScript' -ScriptString $ScriptString
-        $connectionResult = $output.Value[0].Message
+        $connectionResult = $output.Value.Message
         Write-Host "VM: $vmName, Endpoint: $endpoint, Connection-Result: $connectionResult"
     }
 }
