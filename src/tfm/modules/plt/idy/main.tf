bd95ec8b43f5dc376f79477b61dbc247c2cbf27f
devcloud0547 <devcloud0547@outlook.com>
2024-05-07 14:56:52 -0400

    conditional(deployment): add for dcr-ext

--- src/tfm/modules/plt/idy/main.tf
+++ src/tfm/modules/plt/idy/main.tf
@@ -75,6 +75,7 @@ PARAMS
 }
 
 resource "azurerm_management_group_policy_assignment" "ama_initiative_assignment_dcr_ext" {
+  count = data.azurerm_monitor_data_collection_rule.dcr-ext.id != null ? 0 : 1
   name                 = var.ama_initiative_assignment.name_dcr_ext
   policy_definition_id = var.ama_initiative_assignment.policy_set_def_id
   management_group_id  = data.azurerm_management_group.tgt.id
