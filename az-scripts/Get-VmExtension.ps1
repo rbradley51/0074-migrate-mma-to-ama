1bd025a694d766ce0c3967e9befec2324a55a389
devcloud0547 <devcloud0547@outlook.com>
2024-05-10 17:56:06 -0400

    get(extension): fix extension names

--- az-scripts/Get-VmExtension.ps1
+++ az-scripts/Get-VmExtension.ps1
@@ -1,27 +1,29 @@
 Select-AzSubscription -SubscriptionId $env:IDYSUBSCRIPTION
 $vmList = (Get-AzVM -ResourceGroupName $env:rgpName).Name
+$mma = "MicrosoftMonitoringAgent"
+$ama = "AzureMonitorWindowsAgent"
 foreach ($vmName in $vmList) {
     $vmExtensions = (Get-AzVMExtension -ResourceGroupName $env:rgpName -VMName $vmName).Name
-    if (($vmExtensions -contains 'MicrosoftMonitoringAgent') -and ($vmExtensions -contains 'AzureMonitoringWindowsAgent')) {
+    if (($vmExtensions -contains $mma) -and ($vmExtensions -contains $ama)) {
         Write-Host "VM: $vmName, both the MMA AND AMA extensions ARE installed"
         if ($env:removeLegacyAgent -eq "true") 
         {
             Write-Host "Removing MMA extension from VM: $vmName"
             if ($env:removeMMA -eq "true")
             {
-                Remove-AzVMExtension -ResourceGroupName $env:rgpName -VMName $vmName -Name "MicrosoftMonitoringAgent" -Confirm:$false -Force -Verbose
+                Remove-AzVMExtension -ResourceGroupName $env:rgpName -VMName $vmName -Name $mma -Confirm:$false -Force -Verbose
             } 
             else 
             {
                 Write-Host "MMA extension removal skipped for VM: $vmName"
-                Remove-AzVMExtension -ResourceGroupName $env:rgpName -VMName $vmName -Name "MicrosoftMonitoringAgent" -WhatIf -Verbose
+                Remove-AzVMExtension -ResourceGroupName $env:rgpName -VMName $vmName -Name $mma -WhatIf -Verbose
             }
         }
     }
-    elseif (($vmExtensions -contains 'MicrosoftMonitoringAgent') -and !($vmExtensions -contains 'AzureMonitoringWindowsAgent')){
+    elseif (($vmExtensions -contains 'MicrosoftMonitoringAgent') -and !($vmExtensions -contains $ama)){
         Write-Host "VM: $vmName, Only the MMA extension IS installed"
     }
-    elseif (($vmExtensions -contains 'AzureMonitoringWindowsAgent') -and !($vmExtensions -contains 'MicrosoftMonitoringAgent')) 
+    elseif (($vmExtensions -contains $ama) -and !($vmExtensions -contains $mma)) 
     {
         Write-Host "VM: $vmName, Only the AMA extension IS installed"
     }
