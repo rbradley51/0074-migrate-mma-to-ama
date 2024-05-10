29a137f3f8e1c85f59d703f06c769453354f088e
devcloud0547 <devcloud0547@outlook.com>
2024-05-09 20:25:11 -0400

    usupported(properties): remove for dce resource

--- src/tfm/modules/plt/idy/main.tf
+++ src/tfm/modules/plt/idy/main.tf
@@ -73,15 +73,10 @@ resource "azurerm_monitor_data_collection_endpoint" "dce" {
   name                = var.ama_dce.name
   resource_group_name = var.mgt_law.rgp
   location = var.primary_location
-  type = var.ama_dce.type
   public_network_access_enabled = var.ama_dce.public_access
   configuration_access_endpoint = var.ama_dce.cae
   logs_ingestion_endpoint = var.ama_dce.lie
   kind = var.ama_dce.lie
-  identity {
-	type = "UserAssigned"
-	identity_ids = [var.umi_pol_id]
-  }
 }
 
 resource "azurerm_management_group_policy_assignment" "ama_initiative_assignment_dce" {
